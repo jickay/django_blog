1bf30b94aa20e1f924699b958e494b049fb5a64d
jickay <jickay@gmail.com>
2017-10-02 04:58:12 +0000

    add virtual env 3.6

---
+++ testEnv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/jickay/django_blog/testEnv/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ venv36/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/jickay/django_blog/venv36/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
